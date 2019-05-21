f1ba8c6d819906ebbaa3ff6891a1eb2ce980c6c6
aalbina <aalmukhametova@gmail.com>
2019-05-22 01:05:43 +0300

    fix user

---
+++ db/migrate/20190521220457_change_phone_at_user.rb
@@ -0,0 +1,5 @@
+class ChangePhoneAtUser < ActiveRecord::Migration[5.2]
+  def change
+    change_column :users, :phone, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_21_215134) do
+ActiveRecord::Schema.define(version: 2019_05_21_220457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2019_05_21_215134) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_email_notifications_available", default: false
-    t.integer "phone"
+    t.string "phone"
   end
 
 end
